2d4ddef3f860c6cd19e39730506084945f1e97f2
hsun <hang.s@taboola.com>
2024-12-30 11:12:37 -0800

    More updates: icons

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -11,6 +11,11 @@ const projectsData: Project[] = [
     description: `Help you find Bible versers using voice capture.`,
     href: 'https://verse.suns-online.com/',
   },
+  {
+    title: 'Nuxt-Tailwind Starter Blog',
+    description: `A port of Nextjs-Tailwind Starter Blog. Built with Next.js, Tailwind CSS.`,
+    href: 'https://github.com/ooasis/tailwind-nuxtjs-starter-blog',
+  },
 ]
 
 export default projectsData
